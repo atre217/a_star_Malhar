50a213445f5c74058e47f84b929a9f1fdf4cb26f
atre217 <127630498+atre217@users.noreply.github.com>
2023-03-21 23:09:31 -0400

    Update astar_malhar.py

--- astar_malhar.py
+++ astar_malhar.py
@@ -203,4 +203,161 @@ def astar_algo(start,goal,map_width,map_height,step_size,obs_list):
                     
     return par_idx,list_closed,False
                     
+################# BACKTRACKING #################
+def animate_bktrking():
+    for c in itertools.cycle([".", "..", "..."]):
+        if is_computing_backtrack:
+            break
+        sys.stdout.write('\rBacktracking ' + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
 
+
+def backtracking(par_idx,goal,start):
+    back_track = []
+    present= start
+    back_track.append(present)
+    goal_achieved = False
+    global is_computing_backtrack
+    computing = threading.Thread(target=animate_bktrking)
+    computing.start()
+
+    while goal_achieved == False:
+        for coord,parent_cost in par_idx.items():
+            for cost,parent in parent_cost.items():
+                if coord==present:
+                    if parent not in back_track:
+                        back_track.append(present)
+                    present = parent
+                    if parent == goal:
+                        goal_achieved = True
+                        time.sleep(1)
+                        is_computing_backtrack = True
+                        
+                        break
+    back_track.append(goal)
+    return back_track
+
+is_computing_astar_algo = False
+is_computing_backtrack = False
+
+################# Visualizing the map #################
+
+def visualize_map(map_width,map_height,obstacle_scaled,obstacle_cord,list_closed,back_track_coord):
+    obs_map = np.zeros((map_width*2+1,map_height*2+1,3),np.uint8) 
+    obs_map[obstacle_scaled*2]=255
+    obs_set = set(obstacle_scaled)
+    
+    # Using pygame here
+    pygame.init()
+    gameDisplay = pygame.display.set_mode((map_width*2,map_height*2))
+    pygame.surfarray.make_surface(obs_map)
+    pygame.display.set_caption('A-Star Algorithm')
+    
+    gameDisplay.fill((0,0,0))
+    # Adding obstacles 
+    for coords in obs_set:
+        pygame.draw.rect(gameDisplay, (0,0,255), [coords[0]*2 ,abs(250-coords[1])*2,1,1])
+        pygame.display.update()
+    # Adding explored region/ visited nodes
+    for coords in list_closed:
+        pygame.time.wait(10)
+        pygame.draw.rect(gameDisplay, (0,255,0), [coords[0]*2 ,abs(250-coords[1])*2,1,1])
+        pygame.display.update()
+    # Adding back track path
+    for coords in back_track_coord:    
+        pygame.time.wait(10)
+        pygame.draw.rect(gameDisplay, (0,255,255), [coords[0]*2,abs(250-coords[1])*2,1,1])
+        pygame.display.update()
+  
+    pygame.quit()
+
+    obs_map_3d =  np.zeros((map_height+1,map_width+1,3),np.uint8)
+    for x,y in obstacle_cord: 
+        obs_map_3d[(y,x)]=[255,0,255] 
+        
+    for x,y,d in back_track_coord:
+        obs_map_3d[(250-y,x)]=[0,0,255] 
+    backtrack_map = cv2.resize(obs_map_3d,(map_width*3,map_height*3))
+    cv2.imshow('Backtrack Path',backtrack_map) 
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+
+################# MAIN FUNCTION #################
+obstacle_scaled,obstacle_cord =obs_space(600,250)
+
+# Display the map workspace
+x, y = [], []
+for i in obstacle_scaled:
+    x.append(i[0])
+    y.append(i[1])
+plt.scatter(x,y,s=0.1,c='black')
+plt.axis([0,600,0,250])
+plt.title('Obstacle Map')
+plt.grid(which='both')
+plt.show()
+
+try:
+
+    while True: 
+        # getting inputs from the user
+        s_xc = int(input("\nPlease enter the starting x coordinate of the robot : "))
+        s_yc = int(input("Please enter the starting y coordinate of the robot : "))
+        s_orienation =int(input("Please enter the starting orientation of the robot : "))
+
+        g_xc = int(input("\nPlease enter the goal x coordinate of the robot  : "))
+        g_yc = int(input("Please enter the goal y coordinate of the robot  : "))
+        g_orientation =int(input("Please enter the desired final orientation of robot  : "))
+
+        step_size = int(input("\nPlease enter the step size: "))
+        radius = int(input("Please enter the radius of robot : "))
+        clearance = int(input("Please enter the clearance of robot: "))
+        
+        # If inputs are incorrect,
+        if(s_xc>=map_width or g_xc>=map_width or g_yc>=map_height or g_yc>=map_height or s_xc<0 or g_xc<0 or g_yc<0 or g_yc<0):
+            print("Try again: Point out of bounds... 0<x<599 and 0<y<249: ")
+            continue
+        
+        elif((s_xc,s_yc)  in obstacle_cord ) or ((g_xc,g_yc) in obstacle_cord ):
+            print("Try again: The point is on the obstacle space: ")
+            continue
+
+        elif s_orienation not in (0,30,60,-30,-60) or g_orientation not in (0,30,60,-30,-60):
+            print("Try again: Please enter any one of '30,60,-30,-60,0' degrees for Orientation: ")
+            continue
+
+        if radius+ clearance !=10:
+            print("Try again: Enter clearance and radius value not greater than 10: ")
+        
+        if step_size< 1 and step_size>10:
+            print("Try again: Enter step size value between 1 to 10: ")
+            continue           
+
+        else: 
+            start = (s_xc,s_yc,s_orienation) 
+            goal =  (g_xc,g_yc,g_orientation)
+            break
+
+    # keepinga athreshold value for x due to triangle  
+    if s_xc <460 :
+        start_time = time.time()
+        par_idx,list_closed,goal_new,isGoal = astar_algo(start,goal,map_width,map_height,3,obstacle_scaled)
+        # Print final time
+        print("Time explored = %2.3f seconds " % (time.time() - start_time))
+        if(isGoal):
+            back_track_coord =backtracking(par_idx,start,goal_new)
+            print("\nTime required to find Path: ",time.time() - start_time, "seconds" )
+            visualize_map(map_width,map_height,obstacle_scaled,obstacle_cord,list_closed,back_track_coord)
+        
+        else:
+            print("Bactracking Unsuccessful")
+    else :
+        print("Failed to reach goal")
+    
+
+    
+except:
+    print("You have entered an invalid output please Run the program again")
+    
+print("Program Executed ")
